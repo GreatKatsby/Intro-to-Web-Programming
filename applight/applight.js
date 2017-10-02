3efd1eeb2353214f3b9b04fd9ecd42bee58a4bd1
GreatKatsby <yomega3@gmail.com>
2017-10-02 15:39:34 -0400

    Update

--- applight/applight.js
+++ applight/applight.js
@@ -2,12 +2,12 @@ var on = false;
 
 function clicked(){
 	if(on == true){ 
-		alert("off!");
+		document.getElementById("inner").innerHTML = "OFF";
 		document.getElementById("inner").style.cssFloat = "left";
 		document.body.style.backgroundColor = "black";
 		on = false;
 	}else{
-		alert("on");
+		document.getElementById("inner").innerHTML = "ON";
 		document.getElementById("inner").style.cssFloat = "right";
 		document.body.style.backgroundColor = "white";
 		on = true;
