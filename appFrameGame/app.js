a5a8c6c423bc7e0dac48d506445ff4a52b4836e8
GreatKatsby <yomega3@gmail.com>
2017-10-16 16:00:47 -0400

    appFrame Guessing Game

--- appFrameGame/app.js
+++ appFrameGame/app.js
@@ -4,11 +4,62 @@
 function MyApp()
 {
 	var version = "v1.0";
+	var num;
+	var count = 0;
 
 	// creating a private function
 	function setStatus(message)
 	{
 		$("#app>footer").text(message);
+	}
+	
+	function clicked(){
+		document.getElementById("num").disabled = false;
+		document.getElementById("submit").disabled = false;
+		document.getElementById("yes").disabled = true;
+	}//End of clicked
+	
+	function number(){
+		num = parseInt(document.getElementById("num").value);
+		document.getElementById("num").value = null;
+		document.getElementById("guess").disabled = false;
+		document.getElementById("check").disabled = false;
+		document.getElementById("num").disabled = true;
+		document.getElementById("submit").disabled = true;
+	}//End of number
+
+	function checkGuess(guess){	
+		var guess = parseInt(document.getElementById("guess").value);
+		
+		var response = document.getElementById("response");
+	
+		if(guess > num){
+			$("#app>footer").text("Your guess is Too High!");
+			
+			count = count + 1;
+		}//End if
+		if(guess < num){
+			$("#app>footer").text("Your guess is Too Low!");
+					
+			count = count + 1;
+		}//End if
+		if(guess == num){
+			if(count == 1){
+				$("#app>footer").text("Your guess is Correct! It took you " + count + " try!");
+			}//End inner first if
+			
+			if(count > 1){
+				$("#app>footer").text("Your guess is Correct! It took you " + count + " tries!");
+			}//End second inner if
+			
+			document.getElementById("reload").disabled = false;
+			document.getElementById("guess").disabled = true;
+			document.getElementById("check").disabled = true;
+		}//End if
+	}//End checkGuess
+	
+	function reload(){
+		window.location.reload();
 	}
 
 	// creating a public function
@@ -16,6 +67,12 @@ function MyApp()
 	{
 		$("#app>header").append(version);
 		setStatus("ready");
+		
+		$("#yes").on("click", clicked);
+		$("#submit").on("click", number);
+		$("#check").on("click", checkGuess);
+		$("#reload").on("click", reload);
+		
 	};
 } // end MyApp
 
