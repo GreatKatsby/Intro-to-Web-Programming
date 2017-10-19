6d6e7c487624e90dba75e77b284e6c0d2cc1d5ae
GreatKatsby <yomega3@gmail.com>
2017-10-19 18:21:00 -0400

    Task List V1

---
+++ taskAtHand1a/taskAtHand.js
@@ -0,0 +1,59 @@
+"use strict";
+
+// using a function contructor form to create an object
+function TaskAtHandApp()
+{
+	var version = "v1.0";
+
+	// creating a private function
+	function setStatus(message)
+	{
+		$("#app>footer").text(message);
+	}
+
+	// creating a public function
+	this.start = function()
+	{
+		$("#new-task-name").keypress(function(e){
+			if(e.which == 13){
+				addTask();
+				return false;
+			}
+		})
+		.focus();
+		
+		$("#app>header").append(version);
+		setStatus("ready");
+	};
+	
+	function addTask(){
+		var taskName = $("#new-task-name").val();
+		if(taskName){
+			addTaskElement(taskName);
+			
+			//Reset the text field
+			$("#new-task-name").val("").focus();
+		}
+	}
+	
+	function addTaskElement(taskName){
+		var $task = $("<li></li>");
+		$task.text(taskName);
+		$("#task-list").append($task);
+	}
+} // end TaskAtHandApp
+
+/* 	JQuery's shorthand for the document ready event handler
+		could be written: $(document).ready(handler);
+
+		When this page loads, we'll create a global variable
+		named "app" by attaching it to the "window" object
+		(part of the BOM - Browser Object Model)
+*/
+$(function() {
+	window.app = new TaskAtHandApp();
+	window.app.start();
+});
+
+
+
